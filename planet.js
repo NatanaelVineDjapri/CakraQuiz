231e0ba86837769549c07e948adc942a95f761f6
NatanaelVineDjapri <natanaelvinedj@gmail.com>
2025-09-09 22:49:38 +0700

    Add Code

---
+++ planet.js
@@ -0,0 +1,392 @@
+const pertanyaan = {
+  //  🔹 Quiz 1: Planet
+    quiz1:[
+    {
+        pertanyaan: "Apa nama dari planet tersebut …",
+        jawaban: [
+        { text: "Venus", correct: false },
+        { text: "Merkurius", correct: true },
+        { text: "Mars", correct: false },
+        { text: "Pluto", correct: false }
+        ],
+        gambar: "images/infoplanetv2/Q1N1.jpg"
+    },
+    {
+        pertanyaan: "Planet dari gambar disamping dijuluki sebagai 'planet merah'. Apa nama planet tersebut …",
+        jawaban: [
+        { text: "Jupiter", correct: false },
+        { text: "Mars", correct: true },
+        { text: "Saturnus", correct: false },
+        { text: "Uranus", correct: false }
+        ],
+        gambar: "images/infoplanetv2/Q1N2.jpg"
+    },
+    {
+        pertanyaan: "Planet yang memiliki cincin paling mencolok adalah …",
+        jawaban: [
+        { text: "Jupiter", correct: false },
+        { text: "Saturnus", correct: true },
+        { text: "Neptunus", correct: false },
+        { text: "Uranus", correct: false }
+        ]
+    },
+    {
+        pertanyaan: "Planet terpanas di tata surya adalah …",
+        jawaban: [
+        { text: "Merkurius", correct: false },
+        { text: "Venus", correct: true },
+        { text: "Mars", correct: false },
+        { text: "Jupiter", correct: false }
+        ]
+    },
+    {
+        pertanyaan: "Planet dengan jumlah satelit terbanyak adalah …",
+        jawaban: [
+        { text: "Saturnus", correct: true },
+        { text: "Jupiter", correct: false },
+        { text: "Neptunus", correct: false },
+        { text: "Uranus", correct: false }
+        ]
+    },
+    {
+        pertanyaan: "Planet dengan permukaan yang mirip gurun merah adalah …",
+        jawaban: [
+        { text: "Venus", correct: false },
+        { text: "Jupiter", correct: false },
+        { text: "Mars", correct: true },
+        { text: "Saturnus", correct: false }
+        ]
+    },
+    {
+        pertanyaan: "Planet tersebut dekat dengan Bumi, planet apakah itu …",
+        jawaban: [
+        { text: "Mars", correct: false },
+        { text: "Venus", correct: true },
+        { text: "Merkurius", correct: false },
+        { text: "Jupiter", correct: false }
+        ],
+        gambar: "images/infoplanetv2/Q1N7.jpg"
+    },  
+    {
+        pertanyaan: "Planet terbesar di tata surya adalah …",
+        jawaban: [
+        { text: "Jupiter", correct: true },
+        { text: "Saturnus", correct: false },
+        { text: "Uranus", correct: false },
+        { text: "Neptunus", correct: false }
+        ]
+    },
+    {
+        pertanyaan: "Planet yang disebut kembar es adalah …",
+        jawaban: [
+        { text: "Uranus & Neptunus", correct: true },
+        { text: "Jupiter & Saturnus", correct: false },
+        { text: "Bumi & Venus", correct: false },
+        { text: "Mars & Merkurius", correct: false }
+        ]
+    },
+    {
+        pertanyaan: "Dari gambar tersebut, planet itu dekat dengan bintang. Planet apakah itu …",
+        jawaban: [
+        { text: "WASP-12b", correct: true },
+        { text: "ngawi-12jb", correct: false },
+        { text: "GJ-121b", correct: false },
+        { text: "Uranus", correct: false }
+        ],
+        gambar: "images/infoplanetv2/Q1N10.jpg"
+    },
+    ],
+
+  //  🔹 Quiz 2: Bintang dan Galaksi
+    quiz2: [
+    {
+        pertanyaan: "Bintang terdekat dengan Bumi setelah Matahari adalah …",
+        jawaban: [
+        { text: "Sirius", correct: false },
+        { text: "Proxima Centauri", correct: true },
+        { text: "Betelgeuse", correct: false },
+        { text: "Rigel", correct: false }
+        ]
+    },
+    {
+        pertanyaan: "Galaksi tempat Bumi berada adalah …",
+        jawaban: [
+        { text: "Andromeda", correct: false },
+        { text: "Bima Sakti", correct: true },
+        { text: "Sombrero", correct: false },
+        { text: "Triangulum", correct: false }
+        ]
+    },
+    {
+        pertanyaan: "Dari gambar disamping, manakah yang disebut bintang raksasa merah di konstelasi Orion …",
+        jawaban: [
+        { text: "Rigel", correct: false },
+        { text: "Betelgeuse", correct: true },
+        { text: "Sirius", correct: false },
+        { text: "Aldebaran", correct: false }
+        ],
+        gambar: "images/bintag&galaksi/Q2N3.jpg"
+
+    },
+    {
+        pertanyaan: "Gambar tersebut adalah galaksi tetangga yang dekat dengan Bima Sakti, apa nama galaksi tersebut …",
+        jawaban: [
+        { text: "Sombrero", correct: false },
+        { text: "Andromeda", correct: true },
+        { text: "Lilnusnus", correct: false },
+        { text: "Centaurus A", correct: false }
+        ],
+        gambar: "images/bintag&galaksi/Q2N4.jpg"
+    },
+    {
+        pertanyaan: "Rasi bintang yang dikenal sebagai 'Beruang Besar' adalah …",
+        jawaban: [
+        { text: "Orion", correct: false },
+        { text: "Ursa Major", correct: true },
+        { text: "Lyra", correct: false },
+        { text: "Cassiopeia", correct: false }
+        ],
+        gambar: "images/bintag&galaksi/Q2N5.jpg"
+    },
+    {
+        pertanyaan: "Bintang utara (North Star) disebut juga …",
+        jawaban: [
+        { text: "Sirius", correct: false },
+        { text: "Polaris", correct: true },
+        { text: "Vega", correct: false },
+        { text: "Altair", correct: false }
+        ]
+    },
+    {
+        pertanyaan: "Warna bintang ditentukan oleh …",
+        jawaban: [
+        { text: "Komposisi gas", correct: false },
+        { text: "Suhu permukaan", correct: true },
+        { text: "Ukuran", correct: false },
+        { text: "Jarak ke Bumi", correct: false }
+        ]
+    },
+    {
+        pertanyaan: "Nama lain Galaksi Andromeda adalah …",
+        jawaban: [
+        { text: "M51", correct: false },
+        { text: "M31", correct: true },
+        { text: "NGC 1300", correct: false },
+        { text: "Ambaraksi", correct: false }
+        ]
+    },
+    {
+        pertanyaan: "Supernova adalah …",
+        jawaban: [
+        { text: "Bintang baru terbentuk", correct: false },
+        { text: "Bintang yang meledak", correct: true },
+        { text: "Lubang ngawi", correct: false },
+        { text: "Lubang hitam kecil", correct: false }
+        ]
+    },
+    {
+        pertanyaan: "Lubang hitam supermasif di pusat Bima Sakti bernama …",
+        jawaban: [
+        { text: "Cygnus X-1", correct: false },
+        { text: "Sagittarius A*", correct: true },
+        { text: "M87*", correct: false },
+        { text: "Andromeda A", correct: false }
+        ]
+    },
+    ],
+
+  // 🔹 Quiz 3: Roket & Penjelajahan
+    quiz3: [
+    {
+        pertanyaan: "Apa nama dari roket tersebut …",
+        jawaban: [
+        { text: "Falcon 9", correct: false },
+        { text: "Soyuz", correct: false },
+        { text: "Saturn V", correct: true },
+        { text: "Delta IV", correct: false }
+        ],
+        gambar: "images/roket dan penjelajahan/Q3N1.jpg"
+    },
+    {
+        pertanyaan: "Misi Apollo 11 mendarat di Bulan pada tahun …",
+        jawaban: [
+        { text: "1965", correct: false },
+        { text: "1969", correct: true },
+        { text: "1971", correct: false },
+        { text: "1945", correct: false }
+        ]
+    },
+    {
+        pertanyaan: "Gambar disamping adalah orang pertama yang menginjakkan kaki di Bulan, siapa namanya …",
+        jawaban: [
+        { text: "Amba Gagarin", correct: false },
+        { text: "Neil Armstrong", correct: true },
+        { text: "Yuri Gagarin", correct: false },
+        { text: "Nael Armstrong", correct: false }
+        ],
+        gambar: "images/roket dan penjelajahan/Q3N3.jpg"
+    },
+    {
+        pertanyaan: "Satelit buatan pertama yang diluncurkan ke orbit adalah …",
+        jawaban: [
+        { text: "Explorer 1", correct: false },
+        { text: "Sputnik 1", correct: true },
+        { text: "Apollo 1", correct: false },
+        { text: "Vostok 1", correct: false }
+        ],
+        gambar: "images/roket dan penjelajahan/Q3N4.jpg"
+    },
+    {
+        pertanyaan: "Gambar tersebut adalah manusia pertama di luar angkasa, siapakah namanya …",
+        jawaban: [
+        { text: "Neil Armstrong", correct: false },
+        { text: "Yuri Gagarin", correct: true },
+        { text: "mas fuad", correct: false },
+        { text: "ambastronot", correct: false }
+        ],
+        gambar: "images/roket dan penjelajahan/Q3N5.jpg"
+    },
+    {
+        pertanyaan: "ISS adalah singkatan dari …",
+        jawaban: [
+        { text: "Indonesia social serikat", correct: false },
+        { text: "International Space Station", correct: true },
+        { text: "Intergalactic Space System", correct: false },
+        { text: "International Satellite Station", correct: false }
+        ]
+    },
+    {
+        pertanyaan: "Wahana luar angkasa terjauh buatan manusia saat ini adalah …",
+        jawaban: [
+        { text: "Voyager 1", correct: true },
+        { text: "Voyager 2", correct: false },
+        { text: "Pioneer 10", correct: false },
+        { text: "New Horizons", correct: false }
+        ]
+    },
+    {
+        pertanyaan: "Perusahaan swasta yang berhasil mendaratkan roket kembali ke Bumi adalah …",
+        jawaban: [
+        { text: "ambaroket", correct: false },
+        { text: "NASA", correct: false },
+        { text: "SpaceX", correct: true },
+        { text: "Roscosmos", correct: false }
+        ]
+    },
+    {
+        pertanyaan: "Rover yang dikirim NASA ke Mars pada 2021 bernama …",
+        jawaban: [
+        { text: "Curiosity", correct: false },
+        { text: "Opportunity", correct: false },
+        { text: "Spirit", correct: false },
+        { text: "Perseverance", correct: true }
+        ]
+    },
+    {
+        pertanyaan: "Apa nama Teleskop luar angkasa terkenal yang diluncurkan tahun 1990 a…",
+        jawaban: [
+        { text: "James Webb", correct: false },
+        { text: "Hubble", correct: true },
+        { text: "mas faiz", correct: false },
+        { text: "si imut", correct: false }
+        ],
+        gambar: "images/roket dan penjelajahan/Q3N10.jpg"
+    },
+    ],
+
+  // 🔹 Quiz 4: Fenomena Luar Angkasa
+    quiz4: [
+    {
+        pertanyaan: "Apa nama dari fenomena tersebut …",
+        jawaban: [
+        { text: "gerhana bulan", correct: false },
+        { text: "Gerhana matahari", correct: true },
+        { text: "Matahari menutupi Bumi", correct: false },
+        { text: "Matahari redup sendiri", correct: false }
+        ],
+        gambar: "images/fenomena/Q4N1.jpg"
+    },
+    {
+        pertanyaan: "Fenomena partikel bermuatan dari Matahari disebut …",
+        jawaban: [
+        { text: "Cahaya aurora", correct: false },
+        { text: "Angin matahari", correct: true },
+        { text: "Sinar kosmik", correct: false },
+        { text: "Plasma ion", correct: false }
+        ]
+    },
+    {
+        pertanyaan: "Sabuk asteroid terletak di antara planet …",
+        jawaban: [
+        { text: "Bumi & Mars", correct: false },
+        { text: "Saturnus & Uranus", correct: false },
+        { text: "Mars & Jupiter", correct: true },
+        { text: "diantara kau dan a", correct: false }
+        ]
+    },
+    {
+        pertanyaan: "Aurora disebabkan oleh …",
+        jawaban: [
+        { text: "Bulan", correct: false },
+        { text: "Panas Matahari", correct: false },
+        { text: "Interaksi partikel Matahari dengan atmosfer Bumi", correct: true },
+        { text: "Lubang hitam", correct: false }
+        ]
+    },
+    {
+        pertanyaan: "Apa nama dari fase bulan disamping …",
+        jawaban: [
+        { text: "ambasemesta", correct: false },
+        { text: "Gerhana", correct: false },
+        { text: "Bulan purnama", correct: true },
+        { text: "Kuartal pertama", correct: false }
+        ],
+        gambar: "images/fenomena/Q4N5.jpg"
+    },
+    {
+        pertanyaan: "Benda langit yang jatuh ke Bumi disebut …",
+        jawaban: [
+        { text: "Asteroid", correct: false },
+        { text: "ambateroid", correct: false },
+        { text: "Meteor", correct: true },
+        { text: "Planet kecil", correct: false }
+        ]
+    },
+    {
+        pertanyaan: "Ceres diklasifikasikan sebagai planet kerdil sejak …",
+        jawaban: [
+        { text: "2000", correct: false },
+        { text: "2015", correct: true },
+        { text: "2010", correct: false },
+        { text: "2020", correct: false }
+        ]
+    },
+    {
+        pertanyaan: "Teori asal mula alam semesta adalah …",
+        jawaban: [
+        { text: "Teori Kabut", correct: false },
+        { text: "Big Bang", correct: true },
+        { text: "Teori Steady State", correct: false },
+        { text: "Teori Inflasi", correct: false }
+        ]
+    },
+    {
+        pertanyaan: "Lubang hitam terbentuk dari …",
+        jawaban: [
+        { text: "Gas dan debu", correct: false },
+        { text: "Supernova bintang masif", correct: true },
+        { text: "Komet besar", correct: false },
+        { text: "Sisa planet", correct: false }
+        ]
+    },
+    {
+        pertanyaan: "Lapisan luar Matahari disebut …",
+        jawaban: [
+        { text: "Fotosfer", correct: false },
+        { text: "Korona", correct: true },
+        { text: "Kromosfer", correct: false },
+        { text: "Inti", correct: false }
+        ]
+    }
+    ]
+};
---
+++ ui.js
@@ -0,0 +1,31 @@
+const slider = document.querySelector('.statistik-indonesia');
+const semuaSubItem = document.querySelectorAll('.statistik-sub');
+const tombolNext = document.getElementById('next-bott');
+const tombolPrev = document.getElementById('prev-bott');
+
+
+//slider
+let posisi = 0
+const gap = 16;
+const lebar = semuaSubItem[0].offsetWidth;
+
+function geserSlider(){
+    const totalGeser = posisi*(lebar+gap);
+    slider.style.transform=  `translateX(-${totalGeser}px)`;
+}
+
+tombolNext.addEventListener('click',()=>{
+    if (posisi + 5 < semuaSubItem.length){
+        posisi+=5;
+        geserSlider();
+    }
+});
+
+tombolPrev.addEventListener('click' ,() => {
+    if (posisi-5 >=0 ){
+        posisi -= 5;
+        geserSlider();
+
+    }
+})
+
