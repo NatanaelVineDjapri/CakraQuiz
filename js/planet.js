3a0f9846a55ab95b59cd9c3aed5b537e3e4cfc5b
NatanaelVineDjapri <natanaelvinedj@gmail.com>
2025-09-12 10:38:17 +0700

    Fixing Quiz & Folder

---
+++ js/planet.js
@@ -0,0 +1,392 @@
+const pertanyaan = {
+  //  🔹 Quiz 1: Planet
+    quiz1:[
+    {
+        pertanyaan: "Apa nama dari planet dari gambar disamping?",
+        jawaban: [
+        { text: "Venus", correct: false },
+        { text: "Merkurius", correct: true },
+        { text: "Mars", correct: false },
+        { text: "Pluto", correct: false }
+        ],
+        gambar: "../images/Quiz1/Q1N1.jpg"
+    },
+    {
+        pertanyaan: "Planet dari gambar disamping dijuluki sebagai 'planet merah'. Apa nama planet tersebut …",
+        jawaban: [
+        { text: "Jupiter", correct: false },
+        { text: "Mars", correct: true },
+        { text: "Saturnus", correct: false },
+        { text: "Uranus", correct: false }
+        ],
+        gambar: "../images/Quiz1/Q1N2.jpg"
+    },
+    {
+        pertanyaan: "Planet yang memiliki cincin paling mencolok adalah …",
+        jawaban: [
+        { text: "Jupiter", correct: false },
+        { text: "Saturnus", correct: true },
+        { text: "Neptunus", correct: false },
+        { text: "Uranus", correct: false }
+        ]
+    },
+    {
+        pertanyaan: "Planet terpanas di tata surya adalah …",
+        jawaban: [
+        { text: "Merkurius", correct: false },
+        { text: "Venus", correct: true },
+        { text: "Mars", correct: false },
+        { text: "Jupiter", correct: false }
+        ]
+    },
+    {
+        pertanyaan: "Planet dengan jumlah satelit terbanyak adalah …",
+        jawaban: [
+        { text: "Saturnus", correct: true },
+        { text: "Jupiter", correct: false },
+        { text: "Neptunus", correct: false },
+        { text: "Uranus", correct: false }
+        ]
+    },
+    {
+        pertanyaan: "Planet dengan permukaan yang mirip gurun merah adalah …",
+        jawaban: [
+        { text: "Venus", correct: false },
+        { text: "Jupiter", correct: false },
+        { text: "Mars", correct: true },
+        { text: "Saturnus", correct: false }
+        ]
+    },
+    {
+        pertanyaan: "Perhatikan gambar berikut. Planet tersebut berada dekat dengan Bumi. Planet apakah itu …?",
+        jawaban: [
+        { text: "Mars", correct: false },
+        { text: "Venus", correct: true },
+        { text: "Merkurius", correct: false },
+        { text: "Jupiter", correct: false }
+        ],
+        gambar: "../images/Quiz1/Q1N7.jpg"
+    },  
+    {
+        pertanyaan: "Planet terbesar di tata surya adalah …",
+        jawaban: [
+        { text: "Jupiter", correct: true },
+        { text: "Saturnus", correct: false },
+        { text: "Uranus", correct: false },
+        { text: "Neptunus", correct: false }
+        ]
+    },
+    {
+        pertanyaan: "Planet yang disebut kembar es adalah …",
+        jawaban: [
+        { text: "Uranus & Neptunus", correct: true },
+        { text: "Jupiter & Saturnus", correct: false },
+        { text: "Bumi & Venus", correct: false },
+        { text: "Mars & Merkurius", correct: false }
+        ]
+    },
+    {
+        pertanyaan: "Perhatikan gambar planet berikut, planet manakah yang dekat dengan bintang ?",
+        jawaban: [
+        { text: "WASP-12b", correct: true },
+        { text: "ngawi-12jb", correct: false },
+        { text: "GJ-121b", correct: false },
+        { text: "Uranus", correct: false }
+        ],
+        gambar: "../images/Quiz1/Q1N10.jpg"
+    },
+    ],
+
+  //  🔹 Quiz 2: Bintang dan Galaksi
+    quiz2: [
+    {
+        pertanyaan: "Bintang terdekat dengan Bumi setelah Matahari adalah …",
+        jawaban: [
+        { text: "Sirius", correct: false },
+        { text: "Rigel Centauri", correct: false },
+        { text: "Betelgeuse", correct: false },
+        { text: "Proxima Centauri", correct: true }
+        ]
+    },
+    {
+        pertanyaan: "Galaksi tempat Bumi berada adalah …",
+        jawaban: [
+        { text: "Andromeda", correct: false },
+        { text: "Bima Sakti", correct: true },
+        { text: "Sombrero", correct: false },
+        { text: "Triangulum", correct: false }
+        ]
+    },
+    {
+        pertanyaan: "Dari gambar disamping, manakah yang disebut bintang raksasa merah di konstelasi Orion …",
+        jawaban: [
+        { text: "Rigel", correct: false },
+        { text: "Aldebaran", correct: false },
+        { text: "Sirius", correct: false },
+        { text: "Betelgeuse", correct: true }
+        ],
+        gambar: "../images/Quiz2/Q2N3.jpg"
+
+    },
+    {
+        pertanyaan: "Perhatikan gambar berikut. Gambar tersebut menunjukkan galaksi tetangga yang paling dekat dengan Bima Sakti. Apa nama galaksi tersebut?",
+        jawaban: [
+        { text: "Sombrero", correct: false },
+        { text: "Andromeda", correct: true },
+        { text: "Lilnusnus", correct: false },
+        { text: "Centaurus A", correct: false }
+        ],
+        gambar: "../images/Quiz2/Q2N4.jpg"
+    },
+    {
+        pertanyaan: "Rasi bintang yang dikenal sebagai 'Beruang Besar' adalah …",
+        jawaban: [
+        { text: "Orion", correct: false },
+        { text: "Cassiopeia", correct: false },
+        { text: "Lyra", correct: false },
+        { text: "Ursa Major", correct: true }
+        ],
+        gambar: "../images/Quiz2/Q2N5.jpg"
+    },
+    {
+        pertanyaan: "Bintang utara (North Star) disebut juga …",
+        jawaban: [
+        { text: "Sirius", correct: false },
+        { text: "Vega", correct: false },
+        { text: "Polaris", correct: true },
+        { text: "Altair", correct: false }
+        ]
+    },
+    {
+        pertanyaan: "Warna bintang ditentukan oleh …",
+        jawaban: [
+        { text: "Suhu Permukaan", correct: true },
+        { text: "Komposisi Gas", correct: false },
+        { text: "Ukuran", correct: false },
+        { text: "Jarak ke Bumi", correct: false }
+        ]
+    },
+    {
+        pertanyaan: "Nama lain Galaksi Andromeda adalah …",
+        jawaban: [
+        { text: "M51", correct: false },
+        { text: "M31", correct: true },
+        { text: "NGC 1300", correct: false },
+        { text: "Ambaraksi", correct: false }
+        ]
+    },
+    {
+        pertanyaan: "Supernova adalah …",
+        jawaban: [
+        { text: "Bintang baru terbentuk", correct: false },
+        { text: "Bintang yang meledak", correct: true },
+        { text: "Lubang ngawi", correct: false },
+        { text: "Lubang hitam kecil", correct: false }
+        ]
+    },
+    {
+        pertanyaan: "Lubang hitam supermasif di pusat Bima Sakti bernama …",
+        jawaban: [
+        { text: "Cygnus X-1", correct: false },
+        { text: "Andromeda A*", correct: false },
+        { text: "M87*", correct: false },
+        { text: "Sagittarius A*", correct: true }
+        ]
+    },
+    ],
+
+  // 🔹 Quiz 3: Roket & Penjelajahan
+    quiz3: [
+    {
+        pertanyaan: "Apa nama roket dari gambar disamping ?",
+        jawaban: [
+        { text: "Falcon 9", correct: false },
+        { text: "Soyuz", correct: false },
+        { text: "Saturn V", correct: true },
+        { text: "Delta IV", correct: false }
+        ],
+        gambar: "../images/Quiz3/Q3N1.jpg"
+    },
+    {
+        pertanyaan: "Apa nama wahana antariksa pertama yang berhasil mendarat di komet 67P/Churyumov-Gerasimenko?",
+        jawaban: [
+        { text: "Philae", correct: true },
+        { text: "Rosetta", correct: false },
+        { text: "Hayabusa", correct: false },
+        { text: "Cassini", correct: false }
+        ]
+    },
+    {
+        pertanyaan: "Perhatikan gambar berikut. Gambar tersebut menunjukkan astronot yang menjadi orang pertama menginjakkan kaki di Bulan. Siapakah nama astronot tersebut?",
+        jawaban: [
+        { text: "Amba Gagarin", correct: false },
+        { text: "Neil Armstrong", correct: true },
+        { text: "Yuri Gagarin", correct: false },
+        { text: "Nael Armstrong", correct: false }
+        ],
+        gambar: "../images/Quiz3/Q3N3.jpg"
+    },
+    {
+        pertanyaan: "Satelit buatan pertama yang diluncurkan ke orbit adalah …",
+        jawaban: [
+        { text: "Explorer 1", correct: false },
+        { text: "Sputnik 1", correct: true },
+        { text: "Apollo 1", correct: false },
+        { text: "Vostok 1", correct: false }
+        ],
+        gambar: "../images/Quiz3/Q3N4.jpg"
+    },
+    {
+        pertanyaan: "Gambar tersebut adalah manusia pertama di luar angkasa, siapakah namanya …",
+        jawaban: [
+        { text: "Neil Armstrong", correct: false },
+        { text: "Yuri Gagarin", correct: true },
+        { text: "mas fuad", correct: false },
+        { text: "ambastronot", correct: false }
+        ],
+        gambar: "../images/Quiz3/Q3N5.jpg"
+    },
+    {
+        pertanyaan: "ISS adalah singkatan dari …",
+        jawaban: [
+        { text: "Indonesia social serikat", correct: false },
+        { text: "International Space Station", correct: true },
+        { text: "Intergalactic Space System", correct: false },
+        { text: "International Satellite Station", correct: false }
+        ]
+    },
+    {
+        pertanyaan: "Wahana luar angkasa terjauh buatan manusia saat ini adalah …",
+        jawaban: [
+        { text: "Voyager 1", correct: true },
+        { text: "Voyager 2", correct: false },
+        { text: "Pioneer 10", correct: false },
+        { text: "New Horizons", correct: false }
+        ]
+    },
+    {
+        pertanyaan: "Misi luar angkasa manakah yang berhasil mendaratkan wahana di sisi jauh Bulan?",
+        jawaban: [
+        { text: "Apollo 13", correct: false },
+        { text: "Luna 2", correct: false },
+        { text: "Chang'e 4", correct: true },
+        { text: "Chandrayaan-2", correct: false }
+    ]
+    },
+    {
+        pertanyaan: "Apa nama rover yang dikirim NASA ke mars pada tahun 2021 ?",
+        jawaban: [
+        { text: "Curiosity", correct: false },
+        { text: "Opportunity", correct: false },
+        { text: "Spirit", correct: false },
+        { text: "Perseverance", correct: true }
+        ]
+    },
+    {
+        pertanyaan: "Apa nama Teleskop luar angkasa terkenal yang diluncurkan tahun 1990 …",
+        jawaban: [
+        { text: "James Webb", correct: false },
+        { text: "Hubble", correct: true },
+        { text: "Chandra", correct: false },
+        { text: "Spitzer", correct: false }
+        ],
+        gambar: "../images/Quiz3/Q3N10.jpg"
+    },
+    ],
+
+  // 🔹 Quiz 4: Fenomena Luar Angkasa
+    quiz4: [
+    {
+        pertanyaan: "Fenomena ketika Bumi berada di antara Matahari dan Bulan disebut …",
+        jawaban: [
+        { text: "gerhana bulan", correct: false },
+        { text: "Gerhana matahari", correct: true },
+        { text: "Matahari menutupi Bumi", correct: false },
+        { text: "Matahari redup sendiri", correct: false }
+        ],
+        gambar: "../images/Quiz4/Q4N1.jpg"
+    },
+    {
+        pertanyaan: "Fenomena partikel bermuatan dari Matahari disebut …",
+        jawaban: [
+        { text: "Cahaya aurora", correct: false },
+        { text: "Angin matahari", correct: true },
+        { text: "Sinar kosmik", correct: false },
+        { text: "Plasma ion", correct: false }
+        ]
+    },
+    {
+        pertanyaan: "Sabuk asteroid terletak di antara planet …",
+        jawaban: [
+        { text: "Bumi & Mars", correct: false },
+        { text: "Saturnus & Uranus", correct: false },
+        { text: "Mars & Jupiter", correct: true },
+        { text: "Jupiter & Bumi", correct: false }
+        ]
+    },
+    {
+        pertanyaan: "Dibawah ini, manakah yang benar penyebab terjadinya aurora ?",
+        jawaban: [
+        { text: "Bulan", correct: false },
+        { text: "Panas Matahari", correct: false },
+        { text: "Interaksi partikel Matahari dengan atmosfer Bumi", correct: true },
+        { text: "Lubang hitam", correct: false }
+        ]
+    },
+    {
+        pertanyaan: "Fase bulan disamping muncul setiap  …",
+        jawaban: [
+        { text: "15-16 hari sekali", correct: false },
+        { text: "1-300 hari sekali", correct: false },
+        { text: "29-30 hari sekali", correct: true },
+        { text: "25-26 hari sekali", correct: false }
+        ],
+        gambar: "../images/Quiz4/Q4N5.jpg"
+    },
+    {
+        pertanyaan: "Benda langit yang jatuh ke Bumi disebut …",
+        jawaban: [
+        { text: "Asteroid", correct: false },
+        { text: "ambateroid", correct: false },
+        { text: "Meteor", correct: true },
+        { text: "Planet kecil", correct: false }
+        ]
+    },
+    {
+        pertanyaan: "Ceres diklasifikasikan sebagai planet kerdil sejak …",
+        jawaban: [
+        { text: "2000", correct: false },
+        { text: "2015", correct: true },
+        { text: "2010", correct: false },
+        { text: "2020", correct: false }
+        ]
+    },
+    {
+        pertanyaan: "Fenomena kilatan energi sangat besar dari inti galaksi aktif disebut …",
+    jawaban: [
+        { text: "Nebula", correct: false },
+        { text: "Supernova", correct: false },
+        { text: "Quasar", correct: true },
+        { text: "Pulsar", correct: false }
+        ]
+    },
+    {
+        pertanyaan: "Fenomena cincin cahaya akibat pembelokan cahaya oleh gravitasi benda masif disebut …",
+    jawaban: [
+        { text: "Efek Doppler", correct: false },
+        { text: "Lensa gravitasi", correct: true },
+        { text: "Cincin Saturnus", correct: false },
+        { text: "Cahaya aurora", correct: false }
+        ]
+    },
+    {
+        pertanyaan: "Lapisan luar Matahari disebut …",
+        jawaban: [
+        { text: "Fotosfer", correct: false },
+        { text: "Korona", correct: true },
+        { text: "Kromosfer", correct: false },
+        { text: "Inti", correct: false }
+        ]
+    }
+    ]
+};
